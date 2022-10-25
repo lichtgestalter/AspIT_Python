582316247b33f8af03b8d04be49169baecd840e0
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-10-25 20:57:02 +0200

    Initial commit

---
+++ exercise 01.py
@@ -0,0 +1,22 @@
+"""
+læs https://www.freecodecamp.org/news/the-python-handbook/#pythonbasics
+og https://www.freecodecamp.org/news/the-python-handbook/#datatypesinpython
+"""
+
+
+def hello():
+    print("hello world")
+
+
+def type_something():
+    user_input = input("type something:  ")
+    print(user_input)
+
+
+def my_first_function():
+    0
+
+
+hello()
+type_something()
+my_first_function()

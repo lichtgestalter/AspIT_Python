4adb9a011750ca687bc5299ca5b7f3bce7549edc
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-05 20:28:53 +0100

    oop ready

--- S0001.py
+++ S0001.py
@@ -2,12 +2,12 @@
 
 # change the text between the quotation marks
 
-# commit the file to your git repository
+# commit the file to your git repository (commit means: back up the current state of my files as a new version)
     # Git, Commit (or click the green check mark)
     # write a meaningful comment
-    # click on "commit and push"
+    # click on "commit and push" (push means: upload this commit to github)
 
-# check on github that the changed file is present there
+# check in your web browser on github.com that the changed file is present there
 
 # send your teacher the url of your github repository via Teams chat
 # the url looks something like https://github.com/lichtgestalter/S1.git

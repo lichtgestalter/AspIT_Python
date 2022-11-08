a331a0b6b6dacc1371c5a407a7568145938a7982
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-08 15:05:47 +0100

    24

--- S0024.py
+++ S0024.py
@@ -1,32 +1,11 @@
 # Exercise "Number pyramid" (Solution):
 
-# Copy this file into your own solution directory. Write your solution into the copy.
-
-# watch the first 93 seconds of this video: https://www.youtube.com/watch?v=NsjsLwYRW8o
-
-# write a function "pyramid" which produces the numbers shown in the video
-# the function accepts a parameter defining how many number lines to produce
-# the function prints out the numbers of each line and also their sum
-
-# in the main program, call the function with 1, 2, 3, ..., 10 as an argument.
-
-# run the program with shift+f10 (or click on the green arrow)
-
-# add a more general function pyramid2
-# this function accepts as a second parameter a list with the numbers of
-# the pyramid's topmost line
-
-# in the main program, call pyramid2 with 1, 2, 3, ..., 10 as the first argument
-# and a list of numbers of your choice as the second argument.
-# try out different lists as the second argument.
-
-# run the program with shift+f10 (or click on the green arrow)
-
-# if you have no idea how to begin, open S0021.py and start from there
-
-# when your program is complete, push it to your github repository
-# then send this Teams message to your teacher: i am done with exercise "Number pyramid" (in a language of your choice) :-)
+# Inspect this code in detail. Especially if you did not come up with a solution yourself.
+# Find out what every line of code does. For example by changing the code a bit.
+# Then send this Teams message to your teacher: i am done with exercise "Number pyramid" (in a language of your choice) :-)
 # thereafter go on with the next file in numerical order in the teacher's exercise repository after the current exercise.
+
+
 
 
 def pyramid2_uli(lines, start):

b41672971857b8f655c6ce9b16bc049d572ae30c
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-12 22:01:02 +0100

    102

--- S0020.py
+++ S0020.py
@@ -3,23 +3,24 @@ Exercise "Tom the Turtle":
 
 Copy this file into your own solution directory. Write your solution into the copy.
 
-The function demo introduces you to all commands you need, to interact with Tom in the following exercises.
+The function demo introduces you to all commands you need to interact with Tom in the following exercises.
 
 Part 1:
-write a function square which accepts a parameter "length".
-calling this function causes the turtle to draw a square with a side length of "length" pixels.
+Write a function square which accepts a parameter "length".
+Calling this function causes the turtle to draw a square with a side length of "length" pixels.
 
 Part 2:
-write a for loop in the main program, which calls square()
+Write a for loop in the main program, which calls square()
 in order to draw several squares of different sizes in different positions.
 
 Part 3:
-write a function that produces patterns similar to this:
+Write a function that produces patterns similar to this:
 https://pixabay.com/vectors/spiral-square-pattern-black-white-154465/
 
 Part 4:
-write a function that produces patterns similar to this:
+Write a function that produces patterns similar to this:
 https://www.101computing.net/2d-shapes-using-python-turtle/star-polygons/
+The function shall have a parameter, that influences the shape of the pattern.
 
 Part 5:
 Create your own function that produces a cool pattern.
@@ -34,18 +35,18 @@ import turtle    # this imports a library called "random". A library is (someone
 
 
 def demo():  # demonstration of basic turtle commands
+    tom = turtle.Turtle()  # define a variable tom of type Turtle
     tom.speed(1)  # fastest: 10, slowest: 1
     tom.forward(100)  # move 100 pixels
     tom.left(45)  # turn 45 degrees left
     tom.penup()  # do not draw while moving
     tom.forward(50)
     tom.pendown()  # draw while moving
-    tom.pencolor("red")  # from now on, draw in red
+    tom.pencolor("red")  # draw in red
     tom.right(90)  # turn 90 degrees right
     tom.forward(100)
     tom.home()  # return to the original position in the middle of the window
+    turtle.done()  # keeps the turtle window open after the program is done
 
 
-tom = turtle.Turtle()  # define a variable tom of type Turtle
 demo()
-turtle.done()  # keeps the turtle window open after the program is done

a7b52a320cb64c8652686737c643ae16701536ab
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-17 13:46:11 +0100

    todo,  elev reps

--- misc/todo.py
+++ misc/todo.py
@@ -1,11 +1,20 @@
 """
 oplæg
-    *vorstellung uli
-    *installation python etc.
+    vorstellung uli
+    installation python etc.
+    interpreter vs. compiler sprog
     how to use notebooks
     Datatypes
-    Turtlegraphics
-    aufbau programm: imports, klassen, funktionen, hauptprogramm
+    Mandag kl. 14 brætspil
+    Playstation (10er Pauser)
+
+
+    Notebooks durchgehen
+    pycharm intro / Turtlegraphics
+        run current file (shift f10)
+        pull opgaver from GitHub
+        debug with print
+        aufbau programm: imports, klassen, funktionen, hauptprogramm
     debugger erklären
     vor Morris-OOP: kig igen på Tom the turtle
 

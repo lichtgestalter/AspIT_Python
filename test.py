ed054453f05b07092cbc10e7dd6f9eceaf868a02
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-22 10:55:04 +0100

    tan test

---
+++ test.py
@@ -0,0 +1,17 @@
+import math
+
+dx = 200
+dy = 4
+
+print("Expect 359,  1,  181,  179")
+
+print((math.atan2(dy, dx)*180/math.pi))
+print((math.atan2(-dy, dx)*180/math.pi))
+print((math.atan2(dy, -dx)*180/math.pi))
+print((math.atan2(-dy, -dx)*180/math.pi))
+print()
+
+print((math.atan2(dy, dx)*180/math.pi) % 360)
+print((math.atan2(-dy, dx)*180/math.pi) % 360)
+print((math.atan2(dy, -dx)*180/math.pi) % 360)
+print((math.atan2(-dy, -dx)*180/math.pi) % 360)

522b458ca2e2834372e5908ce97a94c0e1ceef6d
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-24 19:09:00 +0100

    opl

--- S0061_rpg1_help.py
+++ S0061_rpg1_help.py
@@ -3,19 +3,19 @@ Exercise: Object oriented role playing game, part 1 :
 
 As always, read the whole exercise description carefully before your start to solve the exercise.
 
-Copy this file into your own solution directory. Edit your copy only.
+Copy this file into your own solution directory. Write your solution into the copy.
 
-define a class Character with attributes name, max_health, _current_health, attackpower.
-_current_health shall be a private attribute, it is not meant to be changed from outside the class
+Define a class "Character" with attributes "name, max_health", "_current_health", "attackpower."
+_current_health shall be a private attribute, it is not meant to be changed from outside the class.
 
-add a constructor (__init__) which accepts the classes' attributes as parameters
-add a method for printing out class objects (__repr__)
+Add a constructor (__init__) which accepts the classes' attributes as parameters.
+Add a method for printing out class objects (__repr__).
 
-add a method hit which reduces _current_health of another character by attackpower.
-example: _current_health=80 and attackpower=10: a hit reduces _current_health to 70.
+Add a method "hit" which reduces _current_health of another character by attackpower.
+Example: _current_health=80 and attackpower=10: a hit reduces _current_health to 70.
 
-the method hit may not change the private attribute _current_health off a (potentially) foreign class
-for this reason we define another method get_hit which reduces _current_health of the object it belongs to by attackpower.
+The method hit may not change the private attribute _current_health of a (potentially) foreign class.
+For this reason we define another method get_hit which reduces _current_health of the object it belongs to by attackpower.
 
 If you get stuck, ask google, the other pupils or the teacher (in this order).
 

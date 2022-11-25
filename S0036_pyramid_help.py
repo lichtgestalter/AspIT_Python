88c647d06f17d430231f50630a84b66f42b7f4b4
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-25 14:37:21 +0100

    kahoot.py

--- S0036_pyramid_help.py
+++ S0036_pyramid_help.py
@@ -5,24 +5,25 @@ As always, read the whole exercise description carefully before your start to so
 
 Copy this file into your own solution directory. Write your solution into the copy.
 
-watch the first 93 seconds of this video: https://www.youtube.com/watch?v=NsjsLwYRW8o
+Watch the first 93 seconds of this video: https://www.youtube.com/watch?v=NsjsLwYRW8o
 
-write a function "pyramid" which produces the numbers shown in the video
-the function accepts a parameter defining how many number rows to produce
-the function prints out the numbers of each row.
+Write a function "pyramid" which produces the numbers shown in the video.
+The function has a parameter defining how many number rows to produce.
+The function prints out the numbers of each row and also their sum.
 
-in the main program, call the function with 1, 2, 3, ..., 10 as an argument
-run the program with shift+f10 (or click on the green arrow)
+In the main program, call the function with 1, 2, 3, ..., 10 as an argument.
 
-add a more general function pyramid2
-this function accepts as a second parameter a list with the numbers of
-the pyramid's topmost row
+Run the program with shift+f10 (or click on the green arrow).
 
-in the main program, call pyramid2 with 1, 2, 3, ..., 10 as the first argument
+Add a more general function pyramid2.
+This function has as a second parameter a list with the numbers of
+the pyramid's topmost row.
+
+In the main program, call pyramid2 with 1, 2, 3, ..., 10 as the first argument
 and a list of numbers of your choice as the second argument.
-try out different lists as the second argument.
+Try out different lists as the second argument.
 
-replace the zeros with your own code
+Replace the zeros in the code below with your own code
 
 When your program is complete, push it to your github repository.
 Then send this Teams message to your teacher: <filename> done
---
+++ oplæg/kahoot.py
@@ -0,0 +1,3 @@
+for i in range(1, 30, 3):
+    if i % 5 == 0:
+        print(i)

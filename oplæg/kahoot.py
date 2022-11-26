2154d373406522750b8e6704537caa3e6469a243
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-26 09:32:17 +0100

    kah

--- oplæg/kahoot.py
+++ oplæg/kahoot.py
@@ -1,3 +1,23 @@
+
+for i in range(3):
+    print(i)
+
+for i in range(1, 30, 3):
+    print(i)
+
+for i in range(1, 30, 3):
+    print(i)
+
+for i in range(1, 30, 3):
+    print(i)
+
+
 for i in range(1, 30, 3):
     if i % 5 == 0:
         print(i)
+
+
+for i in range(1, 30, 3):
+    if i % 5 == 0:
+        print(i)
+

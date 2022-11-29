619d6468ee05a93da7803eccf9eb4dd23a31cc04
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-11-29 21:33:57 +0100

    kah

--- oplæg/kahoot.py
+++ oplæg/kahoot.py
@@ -1,26 +1,66 @@
-for i in range(3):
-    i
+# for i in range(3):
+#     i
+#
+# for i in range(1, 30, 3):
+#     print(i)
+#
+# print()
+#
+#
+# for i in range(1, 30, 3):
+#     if i % 5 == 0:
+#         print(i)
+#
+# print()
+#
+# for i in range(30):
+#     if i % 7 == 0:
+#         print(i)
+#
+# print("x")
+#
+# for i in range(2, 6, -1):
+#     print(i)
+#
 
-for i in range(1, 30, 3):
-    print(i)
+# x = 64.7
+# print(f'{x=}  {type(x)=}')
+#
+# print("-----")
+#
+# x = 64.7
+# x = int(x)
+# print(f'{x=}  {type(x)=}')
+#
+# print("-----")
+#
+# x = 64.7
+# x = str(x)
+# print(f'{x=}  {type(x)=}')
 
-print()
+a = 3
+b = 7
+c = 2
+print((a+b)*c)
 
+print("-----")
 
-for i in range(1, 30, 3):
-    if i % 5 == 0:
-        print(i)
+a = 3
+b = 7
+c = 2
+print(a+b*c)
 
-print()
+print("-----")
 
-for i in range(30):
-    if i % 7 == 0:
-        print(i)
+x = 13
+y = 5
+print(f'{x / y =}   {x // y =}    {x % y =}')
 
-print("x")
+print("-----")
 
-for i in range(2, 6, -1):
-    print(i)
+x = 2
+y = 5
+print(f'{x ** y =}   {type(x ** y) =}')
 
-print("y")
+print("-----")
 

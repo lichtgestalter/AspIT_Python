e1a5a09db960b4133148eb4a032b897a750139c0
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-12-17 21:07:24 +0100

    tk

---
+++ S2000_gui_links.py
@@ -0,0 +1,9 @@
+# grid(): https://tkdocs.com/shipman/grid.html   https://www.tutorialspoint.com/python/tk_grid.htm
+#         https://www.youtube.com/watch?v=BSfbjrqIw20&t=108s (from 1:48 till 7:03)
+
+# tk.Button(): https://tkdocs.com/shipman/button.html   https://www.tutorialspoint.com/python/tk_button.htm#
+# tk.Label(): https://tkdocs.com/shipman/label.html   https://www.tutorialspoint.com/python/tk_label.htm
+# tk.Entry(): https://tkdocs.com/shipman/entry.html   https://www.tutorialspoint.com/python/tk_entry.htm
+# tk.Frame(): https://tkdocs.com/shipman/frame.html   https://www.tutorialspoint.com/python/tk_frame.htm
+# tk.LabelFrame(): https://tkdocs.com/shipman/labelframe.html   https://www.tutorialspoint.com/python/tk_labelframe.htm
+

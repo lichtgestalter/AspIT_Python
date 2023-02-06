25c214950f02cd7388dbd027f16165fe9e884ed0
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-02-06 14:58:14 +0100

    doku

--- milestones/danskcargo/10_S2040_gui_exercise4/danskcargo_gui.py
+++ milestones/danskcargo/10_S2040_gui_exercise4/danskcargo_gui.py
@@ -17,52 +17,52 @@ evenrow = "#cccccc"  # color of even row in treeview
 # endregion global constants
 
 # region container functions
-def read_container_entries():  # Read content of entry boxes
-    return entry_container_id.get(), entry_container_weight.get(), entry_container_destination.get(),
-
-
-def clear_container_entries():  # Clear entry boxes
-    entry_container_id.delete(0, tk.END)  # Delete text in entry box, beginning with the first character (0) and ending with the last character (tk.END)
-    entry_container_weight.delete(0, tk.END)
-    entry_container_destination.delete(0, tk.END)
-    entry_container_weather.delete(0, tk.END)
-
-
-def write_container_entries(values):  # Fill entry boxes
-    entry_container_id.insert(0, values[0])
-    entry_container_weight.insert(0, values[1])
-    entry_container_destination.insert(0, values[2])
-
-
-def edit_container(event, tree):  # Copy selected tuple into entry boxes. Parameter event is mandatory but we don't use it.
-    index_selected = tree.focus()  # Index of selected tuple
-    values = tree.item(index_selected, 'values')  # Values of selected tuple
-    clear_container_entries()  # Clear entry boxes
-    write_container_entries(values)  # Fill entry boxes
-
-
-def read_table(tree, class_):  # fill tree from database
-    count = 0  # Used to keep track of odd and even rows, because these will be colored differently.
-    result = dcsql.select_all(class_)  # Read all containers from database
-    for record in result:
-        if record.valid():  # this condition excludes soft deleted records from being shown in the data table
-            if count % 2 == 0:  # even
-                tree.insert(parent='', index='end', iid=str(count), text='', values=record.convert_to_tuple(), tags=('evenrow',))  # Insert one row into the data table
-            else:  # odd
-                tree.insert(parent='', index='end', iid=str(count), text='', values=record.convert_to_tuple(), tags=('oddrow',))  # Insert one row into the data table
-            count += 1
-
-
+# def read_container_entries():  # Read content of entry boxes
+#     return entry_container_id.get(), entry_container_weight.get(), entry_container_destination.get(),
+#
+#
+# def clear_container_entries():  # Clear entry boxes
+#     entry_container_id.delete(0, tk.END)  # Delete text in entry box, beginning with the first character (0) and ending with the last character (tk.END)
+#     entry_container_weight.delete(0, tk.END)
+#     entry_container_destination.delete(0, tk.END)
+#     entry_container_weather.delete(0, tk.END)
+#
+#
+# def write_container_entries(values):  # Fill entry boxes
+#     entry_container_id.insert(0, values[0])
+#     entry_container_weight.insert(0, values[1])
+#     entry_container_destination.insert(0, values[2])
+#
+#
+# def edit_container(event, tree):  # Copy selected tuple into entry boxes. Parameter event is mandatory but we don't use it.
+#     index_selected = tree.focus()  # Index of selected tuple
+#     values = tree.item(index_selected, 'values')  # Values of selected tuple
+#     clear_container_entries()  # Clear entry boxes
+#     write_container_entries(values)  # Fill entry boxes
+#
+#
+# def read_table(tree, class_):  # fill tree from database
+#     count = 0  # Used to keep track of odd and even rows, because these will be colored differently.
+#     result = dcsql.select_all(class_)  # Read all containers from database
+#     for record in result:
+#         if record.valid():  # this condition excludes soft deleted records from being shown in the data table
+#             if count % 2 == 0:  # even
+#                 tree.insert(parent='', index='end', iid=str(count), text='', values=record.convert_to_tuple(), tags=('evenrow',))  # Insert one row into the data table
+#             else:  # odd
+#                 tree.insert(parent='', index='end', iid=str(count), text='', values=record.convert_to_tuple(), tags=('oddrow',))  # Insert one row into the data table
+#             count += 1
+#
+#
 # endregion container functions
-
+#
 # region common functions
-def refresh_treeview(tree, class_):  # Refresh treeview table
-    empty_treeview(tree)  # Clear treeview table
-    read_table(tree, class_)  # Fill treeview from database
-
-
-def empty_treeview(tree):  # Clear treeview table
-    tree.delete(*tree.get_children())
+# def refresh_treeview(tree, class_):  # Refresh treeview table
+#     empty_treeview(tree)  # Clear treeview table
+#     read_table(tree, class_)  # Fill treeview from database
+#
+#
+# def empty_treeview(tree):  # Clear treeview table
+#     tree.delete(*tree.get_children())
 # endregion common functions
 
 
@@ -94,65 +94,68 @@ tree_container = ttk.Treeview(tree_frame_container, yscrollcommand=tree_scroll_c
 tree_container.grid(row=0, column=0, padx=0, pady=pady)
 tree_scroll_container.config(command=tree_container.yview)
 
-# # Define the data table's formatting and content
-# tree_container['columns'] = ("id", "weight", "destination")  # Define columns
-# tree_container.column("#0", width=0, stretch=tk.NO)  # Format columns. Suppress the irritating first empty column.
-# tree_container.column("id", anchor=tk.E, width=40)  # "E" stands for East, meaning Right. Possible anchors are N, NE, E, SE, S, SW, W, NW and CENTER
-# tree_container.column("weight", anchor=tk.E, width=80)
-# tree_container.column("destination", anchor=tk.W, width=200)
-# tree_container.heading("#0", text="", anchor=tk.W)  # Create column headings
-# tree_container.heading("id", text="Id", anchor=tk.CENTER)
-# tree_container.heading("weight", text="Weight", anchor=tk.CENTER)
-# tree_container.heading("destination", text="Destination", anchor=tk.CENTER)
-# tree_container.tag_configure('oddrow', background=oddrow)  # Create tags for rows in 2 different colors
-# tree_container.tag_configure('evenrow', background=evenrow)
-#
-# tree_container.bind("<ButtonRelease-1>", lambda event: edit_container(event, tree_container))  # Define function to be called, when an item is selected.
-#
-# # Define Frame which contains labels, entries and buttons
-# controls_frame_container = tk.Frame(frame_container)
-# controls_frame_container.grid(row=3, column=0, padx=padx, pady=pady)
-#
-# # Define Frame which contains labels (text fields) and entries (input fields)
-# edit_frame_container = tk.Frame(controls_frame_container)  # Add tuple entry boxes
-# edit_frame_container.grid(row=0, column=0, padx=padx, pady=pady)
-# # label and entry for container id
-# label_container_id = tk.Label(edit_frame_container, text="Id")  # https://www.tutorialspoint.com/python/tk_label.htm
-# label_container_id.grid(row=0, column=0, padx=padx, pady=pady)
-# entry_container_id = tk.Entry(edit_frame_container, width=4, justify="right")  # https://www.tutorialspoint.com/python/tk_entry.htm
-# entry_container_id.grid(row=1, column=0, padx=padx, pady=pady)
-# # label and entry for container weight
-# label_container_weight = tk.Label(edit_frame_container, text="Weight")
-# label_container_weight.grid(row=0, column=1, padx=padx, pady=pady)
-# entry_container_weight = tk.Entry(edit_frame_container, width=8, justify="right")
-# entry_container_weight.grid(row=1, column=1, padx=padx, pady=pady)
-# # label and entry for container destination
-# label_container_destination = tk.Label(edit_frame_container, text="Destination")
-# label_container_destination.grid(row=0, column=2, padx=padx, pady=pady)
-# entry_container_destination = tk.Entry(edit_frame_container, width=20)
-# entry_container_destination.grid(row=1, column=2, padx=padx, pady=pady)
-# # label and entry for container destination
-# label_container_weather = tk.Label(edit_frame_container, text="Weather")
-# label_container_weather.grid(row=0, column=3, padx=padx, pady=pady)
-# entry_container_weather = tk.Entry(edit_frame_container, width=14)
-# entry_container_weather.grid(row=1, column=3, padx=padx, pady=pady)
-#
-# # Define Frame which contains buttons
-# button_frame_container = tk.Frame(controls_frame_container)
-# button_frame_container.grid(row=1, column=0, padx=padx, pady=pady)
-# # Define buttons
-# button_create_container = tk.Button(button_frame_container, text="Create")
-# button_create_container.grid(row=0, column=1, padx=padx, pady=pady)
-# button_update_container = tk.Button(button_frame_container, text="Update")
-# button_update_container.grid(row=0, column=2, padx=padx, pady=pady)
-# button_delete_container = tk.Button(button_frame_container, text="Delete")
-# button_delete_container.grid(row=0, column=3, padx=padx, pady=pady)
-# button_clear_boxes = tk.Button(button_frame_container, text="Clear Entry Boxes", command=clear_container_entries)
-# button_clear_boxes.grid(row=0, column=4, padx=padx, pady=pady)
-#
-# # endregion container widgets
+# Define the data table's formatting and content
+tree_container['columns'] = ("id", "weight", "destination")  # Define columns
+tree_container.column("#0", width=0, stretch=tk.NO)  # Format columns. Suppress the irritating first empty column.
+tree_container.column("id", anchor=tk.E, width=40)  # "E" stands for East, meaning Right. Possible anchors are N, NE, E, SE, S, SW, W, NW and CENTER
+tree_container.column("weight", anchor=tk.E, width=80)
+tree_container.column("destination", anchor=tk.W, width=200)
+tree_container.heading("#0", text="", anchor=tk.W)  # Create column headings
+tree_container.heading("id", text="Id", anchor=tk.CENTER)
+tree_container.heading("weight", text="Weight", anchor=tk.CENTER)
+tree_container.heading("destination", text="Destination", anchor=tk.CENTER)
+tree_container.tag_configure('oddrow', background=oddrow)  # Create tags for rows in 2 different colors
+tree_container.tag_configure('evenrow', background=evenrow)
 
+# tree_container.bind("<ButtonRelease-1>", lambda event: edit_container(event, tree_container))  # Define function to be called, when an item is selected.
+
+# Define Frame which contains labels, entries and buttons
+controls_frame_container = tk.Frame(frame_container)
+controls_frame_container.grid(row=3, column=0, padx=padx, pady=pady)
+
+# Define Frame which contains labels (text fields) and entries (input fields)
+edit_frame_container = tk.Frame(controls_frame_container)  # Add tuple entry boxes
+edit_frame_container.grid(row=0, column=0, padx=padx, pady=pady)
+# label and entry for container id
+label_container_id = tk.Label(edit_frame_container, text="Id")  # https://www.tutorialspoint.com/python/tk_label.htm
+label_container_id.grid(row=0, column=0, padx=padx, pady=pady)
+entry_container_id = tk.Entry(edit_frame_container, width=4, justify="right")  # https://www.tutorialspoint.com/python/tk_entry.htm
+entry_container_id.grid(row=1, column=0, padx=padx, pady=pady)
+# label and entry for container weight
+label_container_weight = tk.Label(edit_frame_container, text="Weight")
+label_container_weight.grid(row=0, column=1, padx=padx, pady=pady)
+entry_container_weight = tk.Entry(edit_frame_container, width=8, justify="right")
+entry_container_weight.grid(row=1, column=1, padx=padx, pady=pady)
+# label and entry for container destination
+label_container_destination = tk.Label(edit_frame_container, text="Destination")
+label_container_destination.grid(row=0, column=2, padx=padx, pady=pady)
+entry_container_destination = tk.Entry(edit_frame_container, width=20)
+entry_container_destination.grid(row=1, column=2, padx=padx, pady=pady)
+# label and entry for container destination
+label_container_weather = tk.Label(edit_frame_container, text="Weather")
+label_container_weather.grid(row=0, column=3, padx=padx, pady=pady)
+entry_container_weather = tk.Entry(edit_frame_container, width=14)
+entry_container_weather.grid(row=1, column=3, padx=padx, pady=pady)
+
+# Define Frame which contains buttons
+button_frame_container = tk.Frame(controls_frame_container)
+button_frame_container.grid(row=1, column=0, padx=padx, pady=pady)
+# Define buttons
+button_create_container = tk.Button(button_frame_container, text="Create")
+button_create_container.grid(row=0, column=1, padx=padx, pady=pady)
+button_update_container = tk.Button(button_frame_container, text="Update")
+button_update_container.grid(row=0, column=2, padx=padx, pady=pady)
+button_delete_container = tk.Button(button_frame_container, text="Delete")
+button_delete_container.grid(row=0, column=3, padx=padx, pady=pady)
+button_clear_boxes = tk.Button(button_frame_container, text="Clear Entry Boxes")
+# button_clear_boxes = tk.Button(button_frame_container, text="Clear Entry Boxes", command=clear_container_entries)
+button_clear_boxes.grid(row=0, column=4, padx=padx, pady=pady)
+
+# endregion container widgets
+
+# region main program
 if __name__ == "__main__":  # Executed when invoked directly. We use this so main_window.mainloop() does not keep our unit tests from running.
-    refresh_treeview(tree_container, dcd.Container)  # Load data from database
+    # refresh_treeview(tree_container, dcd.Container)  # Load data from database
     main_window.mainloop()  # Wait for button clicks and act upon them
+# endregion main program
 

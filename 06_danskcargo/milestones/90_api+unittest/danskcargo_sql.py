7bb0d795fdedb970419fd4bca4eea595e33e70f4
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-02-13 18:18:40 +0100

    mapper reorg

--- 06_danskcargo/milestones/90_api+unittest/danskcargo_sql.py
+++ 06_danskcargo/milestones/90_api+unittest/danskcargo_sql.py
@@ -5,7 +5,7 @@ from datetime import date
 
 from danskcargo_data import Container, Aircraft, Transport, Base
 
-Database = 'sqlite:///06_danskcargo.db'  # first part: database type, second part: file path
+Database = 'sqlite:///danskcargo.db'  # first part: database type, second part: file path
 
 
 def create_test_data():  # Optional. Used to test data base functions before gui is ready.

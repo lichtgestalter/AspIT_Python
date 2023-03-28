d3353d7e934fed3c1478b39d165199190cc6c029
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-03-28 13:41:56 +0200

    oop

--- 02_oop/S0570_oop_terms.py
+++ 02_oop/S0570_oop_terms.py
@@ -22,7 +22,24 @@ class ClassName:
         print("Don't call this method from outside this class!")
 
 
+class ClassName2(ClassName):  # ClassName2 inherits all methods and attributes from ClassName
+
+    def method1(self):
+        print("this is another method1")
+
+
+print("Part 1")
 object1 = ClassName(4, 160)  # creates an instance/object by calling the constructor __init__
 object1.method1()  # calls method1 on object1
 object1._protected_method()  # don't do this
 print(object1)  # calls internally __repr__ on object1
+
+print("\nPart 2")
+object2 = ClassName2(12, 99)  # creates an instance of class ClassName2.  ClassName2 has own function __init__, so it uses the inherited __init__ of class ClassName
+print(object2)  # calls internally __repr__ on object1
+object2.method1()  # calls method1 on object2
+
+print("\nPart 3")
+objectlist = [object1, object2]
+for obj in objectlist:
+    obj.method1()  # calls different versions of method1(). Example for polymorphism.

440ed763a0eacd7cff6571071309b4b9062bd925
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-03-29 09:15:56 +0200

    oop

--- 02_oop/S0570_oop_terms.py
+++ 02_oop/S0570_oop_terms.py
@@ -15,31 +15,31 @@ class ClassName:
         return f"ClassName: {self.attribute1=} {self.attribute2=}"
 
     def method1(self):
-        print("this is method1")
-        self._protected_method()
+        print("This is method1")
+        self._protected_method()  # Calling the protected method from inside its class. This is ok.
 
     def _protected_method(self):  # protected method or attribute names always start with one underscore
-        print("Don't call this method from outside this class!")
+        print("This is a protected method. Call it only from inside its class.")
 
 
 class ClassName2(ClassName):  # ClassName2 inherits all methods and attributes from ClassName
 
     def method1(self):
-        print("this is another method1")
+        print("This is another method1")
 
 
-print("Part 1")
+print("--- Class: ClassName, Object: Object1 ---")
 object1 = ClassName(4, 160)  # creates an instance/object by calling the constructor __init__
 object1.method1()  # calls method1 on object1
 object1._protected_method()  # don't do this
 print(object1)  # calls internally __repr__ on object1
 
-print("\nPart 2")
+print("\n--- Class: ClassName2, Object: Object2 ---")
 object2 = ClassName2(12, 99)  # creates an instance of class ClassName2.  ClassName2 has own function __init__, so it uses the inherited __init__ of class ClassName
 print(object2)  # calls internally __repr__ on object1
 object2.method1()  # calls method1 on object2
 
-print("\nPart 3")
+print("\n--- Example for polymorphism ---")
 objectlist = [object1, object2]
 for obj in objectlist:
     obj.method1()  # calls different versions of method1(). Example for polymorphism.

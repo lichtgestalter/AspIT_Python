f849b23995e7a2723f2e67ba8af95b70b6feb497
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-04-13 14:57:05 +0200

    EN->DK

--- 01_basics/S0120_factorial_exercise.py
+++ 01_basics/S0120_factorial_exercise.py
@@ -7,25 +7,25 @@ Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
 
 Alt, hvad du har brug for at vide om funktioner for at løse denne øvelse, finder du i jupiter-notesbogen S0040_basics.ipynb.
 
-Write a function named "factorial".
-Scroll down to find the place in this file, where to write the function in.
+Skriv en funktion med navnet "factorial".
+Rul ned for at finde det sted i denne fil, hvor du skal skrive funktionen ind.
 
-The function factorial shall ...
-    have one parameter named "number".
-    calculate the factorial of this parameter and store the result in a variable named result.
-        (If you do not remember the definition of factorial: https://da.wikipedia.org/wiki/Fakultet_(matematik) or https://en.wikipedia.org/wiki/Factorial)
-    return the result.
+Funktionen factorial skal ...
+    have én parameter ved navn "number".
+    beregne fakultetstallet for denne parameter og gemme resultatet i en variabel med navnet " result ".
+    returnér resultatet.
+    (Hvis du ikke kan huske definitionen af fakultet: https://da.wikipedia.org/wiki/Fakultet_(matematik) eller https://en.wikipedia.org/wiki/Factorial)
 
-In the main program...
-    Write a line of code saving the integer number 5 in a variable called factorial_input.
-    In the next line of code call the function factorial with factorial_input as its argument
-    and save the result of this function call in a variable named factorial_result.
-    In the next line of code print out "5! = 120" using the variables factorial_input and factorial_result.
+I hovedprogrammet...
+    Skriv en kodelinje, der gemmer det integer tal 5 i en variabel kaldet factorial_input.
+    I den næste kodelinje kalder du funktionen factorial med factorial_input som argument
+    og gem resultatet af dette funktionskald i en variabel med navnet factorial_result.
+    I den næste kodelinje udskrives "5! = 120" ved hjælp af variablerne factorial_input og factorial_result.
 
-Run the program. "5! = 120" should be printed in the console.
-Change the first line of the main program to "factorial_input = 6".
-Run the program again.
-This time "6! = 720" should be printed in the console.
+Kør programmet. "5! = 120" skal udskrives i konsollen.
+Ændr den første linje i hovedprogrammet til "factorial_input = 6".
+Kør programmet igen.
+Denne gang skal "6! = 720" udskrives i konsollen.
 
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig

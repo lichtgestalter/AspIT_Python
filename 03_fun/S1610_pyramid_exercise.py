b47d6230abb30f9ae9e7ad396fb3a4e2f5b27c78
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-06-01 13:55:57 +0200

    .

--- 03_fun/S1610_pyramid_exercise.py
+++ 03_fun/S1610_pyramid_exercise.py
@@ -2,6 +2,9 @@
 
 Som altid skal du læse hele øpgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
 
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
 Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
 
 Se de første 93 sekunder af denne video: https://www.youtube.com/watch?v=NsjsLwYRW8o

b9c58905417ef89f70a1d2bb5c1507a93e9e7c25
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-06-12 11:30:35 +0200

    pyramid opgavestilling

--- 03_fun/S1620_pyramid_help.py
+++ 03_fun/S1620_pyramid_help.py
@@ -2,32 +2,32 @@
 
 Som altid skal du læse hele øpgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
 
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
 Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
 
 Se de første 93 sekunder af denne video: https://www.youtube.com/watch?v=NsjsLwYRW8o
 
 Skriv en funktion "pyramid", der producerer de tal, der er vist i videoen.
-Funktionen har en parameter, der definerer, hvor mange talrækker der skal produceres.
+Funktionen har en parameter "lines", der definerer, hvor mange talrækker der skal produceres.
 Funktionen udskriver tallene i hver række og også deres sum.
 
 I hovedprogrammet kalder du funktionen med fx 7 som argument.
 
 Tilføj en mere generel funktion pyramid2.
-Denne funktion har som andet parameter en liste med tallene i
-pyramidens øverste række.
+Denne funktion har som andet parameter "firstline" en liste med pyramidens øverste rækkens tallene.
 
-I hovedprogrammet kalder du pyramid2 med 1, 2, 3, ..., 10 som det første argument
-og en liste med tal efter eget valg som det andet argument.
+I hovedprogrammet kalder du pyramid2 med fx 10 som det første argument
+og en liste med tal efter eget valg som andet argument.
 Afprøv forskellige lister som andet argument.
 
-Erstat nuller i nedenstående kode med din egen kode.
-
-Hvis du går i stå, kan du spørge google, de andre elever eller læreren (i denne rækkefølge).
-Hvis du sidder stadig fast, skal du åbne S1630_pyramid_solution.py og sammenligne den med din løsning.
+Hvis du ikke aner, hvordan du skal begynde, kan du åbne S1620_pyramid_help.py og starte derfra
 
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
+
 
 
 def pyramid(lines):

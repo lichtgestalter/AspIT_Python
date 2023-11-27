ac63306a9dec5b025f2c4c467701ebd080af98cd
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2023-11-27 12:00:26 +0100

    0105 tips

---
+++ 01_basics/S0105_pycharm_tips.py
@@ -0,0 +1,50 @@
+"""
+Her er nogle nyttige keyboard shortcuts for pycharm.
+Vend tilbage til denne fil en gang i mellem og prøv nogle shortcuts.
+
+Du kan finde mere shortcuts her:
+https://www.jetbrains.com/help/idea/mastering-keyboard-shortcuts.html
+
+Alt+Down/Up: move line down/up
+Ctrl+Ins: insert line
+Ctrl+Del: delete line
+Ctrl+D : duplicate line
+
+Alt+Shift+mousedrag: rectangular selection
+Ctrl+W / Ctrl+Shift+W: expand / reduce selection
+
+Ctrl+1: comment / uncomment
+Ctrl+Shift+(Numpad)Minus: collapse all (folding of blocks, functions, classes, etc.)
+Ctrl+Shift+(Numpad)Plus: expand all
+
+Shift+F6: refactor (change names etc.)
+Ctrl+Shift+A: search IDE commands
+Ctrl+Alt+S: settings
+Ctrl+B: show usages
+Ctrl+Alt+Shift+F8: remove all breakpoints
+
+"""
+
+# Den følgende kode tjener bare som legeplads hvor du kan afprøve nogle af de ovenstående shortcuts.
+
+def eksempel(lines, firstline):
+    strings = firstline.split(" ")  # split firstline into a list of strings
+    numbers = []  # numbers is a list and will later contain numbers
+    for s in strings:
+        if len(s) > 0:
+            numbers.append(int(s.strip()))  # strip removes white spaces and similar characters from the beginning and end of string
+    number_lists = [numbers]  # number_lists is a list and will later contain lists of numbers
+    for line in range(lines):
+        number_lists.append(number_lists[line].copy())  # copy the last element in number_lists and append it to number_lists
+        print("row " + str(line+1), end=": ")
+        print(number_lists[line])
+        index_shift = 0  # the last line keeps growing while we edit it. this variable helps us to keep track where we have to insert the next number.
+        for n in range(len(number_lists[line])-1):
+            if number_lists[line][n] + number_lists[line][n + 1] == line + 2:  # is the criterion for insertion of a new number met?
+                number_lists[line+1].insert(n + index_shift + 1, line + 2)
+                index_shift += 1
+
+firstline_input = input("Enter the first row of the pyramid. (Separate the individual numbers with spaces.): ")
+lines_input = int(input("Enter the number of rows to print: "))
+eksempel(lines_input, firstline_input)
+print("0123456789")

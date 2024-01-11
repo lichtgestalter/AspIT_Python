2bd21ce811c34d15e92dd163df8a663f34b99ebd
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-01-11 21:48:56 +0100

    .

--- 03_fun/S1520_turtle_hunt_service.py
+++ 03_fun/S1520_turtle_hunt_service.py
@@ -17,6 +17,15 @@ def direction(start_position, end_position):
     # returns the direction from start_position to end_position in degrees
     # 0° is east (plus x-axis), which is also the direction of each turtle at the beginning of each hunt.
     # 90° is south (minus y-axis), 180° is west (minus x-axis), 270° is north (plus y-axis)
+    #
+    #           270°
+    #            N
+    #            |
+    #   180° W --o-- E 0°
+    #            |
+    #            S
+    #           90°
+    #
     delta_x = end_position[0] - start_position[0]
     delta_y = end_position[1] - start_position[1]
     angle = math.atan2(delta_y, delta_x) * 180 / math.pi

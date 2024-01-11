f46df6c726f5f90b1a687fd2c8b06f84544426a7
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-01-11 11:17:13 +0100

    .

---
+++ 03_fun/S1730_lunar_arithmetic_exercise.py
@@ -0,0 +1,33 @@
+# https://www.youtube.com/watch?v=cZkGeR9CWbk
+
+
+"""Opgave "Lunar arithmetic"
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Del 1:
+    Se de første 3 minutter af denne video:
+    https://www.youtube.com/watch?v=cZkGeR9CWbk
+
+Del 2:
+    Skriv en klasse Lunar_int(), med metoder, der gør, at du kan anvende operatorerne + og * på
+    objekter af denne klasse, og at resultaterne svarer til de regler, der forklares i videoen.
+
+Del 3:
+    Se resten af videoen.
+
+Del 4:
+    Skriv en funktion calc_lunar-primes(n), som retunerer en liste med de første n lunar primes.
+
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil."""
+
+
+

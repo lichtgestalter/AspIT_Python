62c58eefbcf3669c0c2db2e501200b1104bda792
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2024-06-04 13:15:44 +0200

    .

---
+++ 01_basics/S0115_minimum_exercise.py
@@ -0,0 +1,28 @@
+"""
+Opgave "minimum":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Skriv en funktion med navnet "minimum".
+
+Funktionen minimum skal ...
+    have én parameter ved navn "numberlist".
+    ikke bruge pythons inbyggede funktion min.
+    returnere den mindste tal som er i numberlist.
+
+Der er allerede én kodelinje i hovedprogrammet, der kalder denne funktion.
+
+Kør programmet. Først 2 og derefter 1 bør udskrives i konsollen. Får du de rigtige resultater?
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil.
+"""
+
+
+print(minimum([6, 2, 7, 3]))  # Should print 2
+print(minimum([8, 1, 17, 3, 5, 8, 3]))  # Should print 1

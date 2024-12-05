e4d02a628266162f60638611ecd330b48dd39e2e
lichtgestalter <uli.scheuss@gmail.com>
2024-12-05 14:34:45 +0100

    .

---
+++ 01_basics/S0108_research_help.py
@@ -0,0 +1,16 @@
+"""
+
+Her er nogle ressourcer, som kan hjælpe dig, når du sidder fast.
+
+CTRL+klik på de følgende links.
+
+Chatbots:
+    https://gemini.google.com/app
+    https://chatgpt.com/
+    https://www.perplexity.ai/  (Giver ikke bare svar, men kommer også med links så man kan tjekke om svaret er rigtig.)
+
+Python specifik:
+    https://www.w3schools.com/python/  (Gode, korte forklaringer til Python basics)
+
+
+"""

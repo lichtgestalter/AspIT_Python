787ee08f3e5d96af6e41b69169cb7fcc0ac11598
lichtgestalter <uli.scheuss@gmail.com>
2025-01-22 14:23:58 +0100

    .

--- 01_basics/S0100_test.py
+++ 01_basics/S0100_test.py
@@ -17,7 +17,8 @@ I konsollen burde vises:
 I programkoden ændre teksten mellem anførselstegnene.
 Kør programmet igen.
 
-Commit filen til dit eget git-repository (commit betyder: sikkerhedskopier den aktuelle tilstand af dine filer som en ny version)
+Commit filen til dit eget git-repository (commit betyder: sikkerhedskopier den aktuelle tilstand
+af dine filer som en ny version)
     - klik på "Git", og derefter på "Commit" (eller CTRL+K)
     - skriv en meningsfuld kommentar
     - klik på "commit and push" (push betyder: upload dette commit til github)
@@ -25,9 +26,11 @@ Commit filen til dit eget git-repository (commit betyder: sikkerhedskopier den a
 
 Kontroller i din webbrowser på github.com, at den ændrede fil er til stede i dit repository på github.
 
-På GitHub-websiden for dit repository skal du klikke på "Settings" (indstillinger) og sikre dig, at dit repository er offentligt (General, Change repository visibility).
+På GitHub-websiden for dit repository skal du klikke på "Settings" (indstillinger) og sikre dig,
+at dit repository er offentligt (General, Change repository visibility).
 
-Hvis du ikke allerede har gjort det, skal du sende din lærer url'en til dit github-løsningsrepositorium via Teams-chat.
+Hvis du ikke allerede har gjort det, skal du sende din lærer url'en til dit
+github-løsningsrepositorium via Teams-chat.
 URL'en ser ud som https://github.com/dit_brugernavn/Solutions.git
 
 Derefter går du videre med den næste fil.

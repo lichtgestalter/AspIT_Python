425e6da0507dde1789d290b54d41e8ac057892da
bisaek <huggerbertram@gmail.com>
2025-01-23 13:41:16 +0100

    S0750

--- 02_oop/S0750_rpg2_exercise.py
+++ 02_oop/S0750_rpg2_exercise.py
@@ -31,3 +31,133 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> done
 Fortsæt derefter med den næste fil."""
 
+from random import choice, random
+
+from six import print_
+
+
+class Character:
+    def __init__(self, name, max_health, attackpower):
+        self.poisoned = 0
+        self.name = name
+        self.max_health = max_health
+        self._current_health = max_health
+        self.attackpower = attackpower
+
+    def __repr__(self):
+        return f"name: {self.name}, current_health: {self._current_health}/{self.max_health}, attackpower: {self.attackpower}"
+
+    def hit(self, character):
+        character.get_hit(self)
+
+    def get_hit(self, character):
+        self._current_health -= character.attackpower
+
+    def get_healed(self, character):
+        self._current_health += character.healpower
+
+    def get_poisoned(self, character):
+        self.poisoned += character.attackpower
+
+    def dead(self):
+        return self._current_health <= 0
+
+    def turn(self):
+        self._current_health -= self.poisoned
+        if self.poisoned > 0:
+            self.poisoned -= 1
+
+
+class Healer(Character):
+    def __init__(self, name, max_health, healpower):
+        super().__init__(name, max_health, 0)
+        self.healpower = healpower
+
+    def __repr__(self):
+        return f"{super().__repr__()[:-16]}, healpower: {self.healpower}"
+
+    def heal(self, character):
+        character.get_healed(self)
+
+
+class Magician(Character):
+    def poison(self, character):
+        character.get_poisoned(self)
+
+class Hunter(Character):
+
+    def __init__(self, name, max_health, attackpower, hit_chance):
+        super().__init__(name, max_health, attackpower)
+        self.hit_chance = hit_chance
+
+    def hit(self, character):
+        if random() < self.hit_chance:
+            character.get_hit(self)
+
+
+class Game:
+    def __init__(self, characters):
+        self.loser = None
+        self.characters = characters
+
+    def print_game(self):
+
+        for character in self.characters:
+            print(character)
+
+        print()
+
+    def random_character(self, character=None):
+        characters = list(self.characters)
+        if character is not None:
+            characters.remove(character)
+        return choice(characters)
+
+    def dead(self):
+        for character in self.characters:
+            if character.dead():
+                return character.name
+        return False
+
+    def turn(self, func):
+        while not self.dead():
+            for character in self.characters:
+                character.turn()
+            func()
+        self.loser = self.dead()
+
+
+lost = {}
+
+for match in range(100):
+    hero1 = Character("hero 1", 1000, 10)
+    hero2 = Character("hero 2", 500, 20)
+
+    magician1 = Magician("magician 1", 600, 5)
+    magician2 = Magician("magician 2", 400, 10)
+
+    hunter1 = Hunter("hunter 1", 500, 50, 0.5)
+
+    healer1 = Healer("healer 1", 500, 50)
+
+    game = Game([hero1, hero2, magician1, magician2, hunter1, healer1])
+
+    @game.turn
+    def turn():
+        hero1.hit(game.random_character(hero1))
+        hero2.hit(game.random_character(hero2))
+        magician1.poison(game.random_character(magician1))
+        magician2.poison(game.random_character(magician2))
+        hunter1.hit(game.random_character(hunter1))
+        healer1.heal(game.random_character())
+
+
+    if game.loser in lost:
+        lost[game.loser] += 1
+    else:
+        lost[game.loser] = 1
+lost = dict(sorted(lost.items()))
+
+for x in lost:
+    print(f"{x}: {lost[x]}")
+

2347417dfec6a9e53f678aa87f2f6dc60d181d71
lichtgestalter <uli.scheuss@gmail.com>
2025-01-25 01:41:19 +0100

    .

--- 30_danskcargo/milestones/90_api+unittest/weather.py
+++ 30_danskcargo/milestones/90_api+unittest/weather.py
@@ -1,19 +1,23 @@
-import json
-import requests  # pip install requests
+import httpx  # pip install httpx
 
 KEY = "2a3891ce1248786a1398a888debb0368"  # ulsc@aspit.dk
 
 
 def weather_now(city, key=KEY):
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + key
-    response = requests.get(url)  # Some background information: https://www.w3schools.com/python/ref_requests_get.asp
-    weather = json.loads(response.text)  # https://www.w3schools.com/python/python_json.asp  Deserialize json into a hierarchy of dictionaries and lists
+    response = httpx.get(url)
+    weather = response.json()  # Deserialize json into a hierarchy of dictionaries and lists
+
+    if weather["cod"] == "404":
+        weather_report = "Unknown Location"
+        return weather_report
+
     print(f'{weather=}')
     print(f'{weather["weather"]=}')
     print(f'{weather["weather"][0]["main"]=}')
     print(f'{int(weather["main"]["temp"])=}')
     if weather["cod"] == 200:
-        weather_report = weather["weather"][0]["main"] + ", " + str(int(weather["main"]["temp"])) + "°C"
+        weather_report = city + ": " + weather["weather"][0]["main"] + ", " + str(int(weather["main"]["temp"])) + "°C"
     else:
         weather_report = "Unknown Location"
     return weather_report

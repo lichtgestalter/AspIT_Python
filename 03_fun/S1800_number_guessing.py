ba640d3a109f02c2f82459db32bc286bb04db84d
bisaek <huggerbertram@gmail.com>
2025-02-03 11:53:35 +0100

    S1800

--- 03_fun/S1800_number_guessing.py
+++ 03_fun/S1800_number_guessing.py
@@ -19,3 +19,30 @@ Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
+
+from random import randint
+
+number = list(map(int, str(randint(1000, 9999))))
+guess = []
+guesses = 0
+
+while guess != number:
+    guess = list(map(int, input("Guess a number ")))
+    guesses += 1
+
+    black_coin = 0
+    white_coin = 0
+
+    for i in range(len(guess)):
+        if number[i] == guess[i]:
+            black_coin += 1
+        elif number.count(guess[i]) >= 1:
+            white_coin += 1
+
+    print(f"sorte mønter: {black_coin}")
+    print(f"hvide mønter: {white_coin}")
+
+
+print(f"du gættede rigtet på {guesses} forsøg")
+
+

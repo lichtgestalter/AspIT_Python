b396bf8336bd99d7c8a785241e593959a3c4e2e5
bisaek <huggerbertram@gmail.com>
2025-02-03 13:11:34 +0100

    S2010

--- 10_gui/S2010_gui_exercise1.py
+++ 10_gui/S2010_gui_exercise1.py
@@ -11,3 +11,26 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 
+import tkinter as tk
+
+padx = 8
+pady = 8
+
+main_window = tk.Tk()
+main_window.title('my first GUI')
+main_window.geometry("500x500")
+
+frame = tk.LabelFrame(main_window, text="Container")
+frame.grid(column=0, row=0)
+
+label = tk.Label(frame, text="id")
+label.grid(column=0, row=0, padx=padx, pady=pady)
+
+entry = tk.Entry(frame, width=4)
+entry.grid(column=0, row=1, padx=padx, pady=pady)
+
+button = tk.Button(frame, text="Create")
+button.grid(column=0, row=2, padx=padx, pady=pady)
+
+
+main_window.mainloop()

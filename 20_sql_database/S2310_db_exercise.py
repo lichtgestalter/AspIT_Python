eb7a7d39589eb1af578898aa1008035d50fb510a
bisaek <huggerbertram@gmail.com>
2025-02-04 10:53:31 +0100

    S2310

--- 20_sql_database/S2310_db_exercise.py
+++ 20_sql_database/S2310_db_exercise.py
@@ -33,3 +33,61 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil.
 """
+
+from sqlalchemy.orm import declarative_base, Session  # install sqlalchemy with the command "pip install SQLAlchemy" in a terminal.
+from sqlalchemy import Column, String, Integer  # the library sqlalchemy helps us to work with a database
+from sqlalchemy import create_engine, select
+
+Database = 'sqlite:///S2311_my_second_sql_database.db'
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    age = Column(Integer)
+
+    def __repr__(self):
+        return f"Customer({self.name=}, {self.address=}, {self.age=})"
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    product_number = Column(Integer)
+    price = Column(Integer)
+    brand = Column(String)
+
+    def __repr__(self):
+        return f"Product({self.product_number=}, {self.price=}, {self.brand=})"
+
+def create_test_data():
+    with Session(engine) as session:
+        session.add_all([
+            Customer(name="Jan", address="denmark", age=18),
+            Customer(name="Peter", address="denmark", age=28),
+            Customer(name="Harry", address="denmark", age=23),
+            Product(product_number=1000, price=5000, brand="Apple"),
+            Product(product_number=1001, price=4000, brand="Samsung")
+        ])
+        session.commit()
+
+def select_all(classparam):
+    with Session(engine) as session:
+        records = session.scalars(select(classparam))
+        result = []
+        for record in records:
+            result.append(record)
+    return result
+
+
+def get_record(classparam, record_id):
+    with Session(engine) as session:
+        record = session.scalars(select(classparam).where(classparam.id == record_id)).first()
+    return record
+
+engine = create_engine(Database, echo=False, future=True)
+Base.metadata.create_all(engine)
+
+print(select_all(Customer))
+print(select_all(Product))

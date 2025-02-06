6c294a7e9bcc40a3491cd67161d694013086b315
bisaek <huggerbertram@gmail.com>
2025-02-06 10:47:50 +0100

    milestone 2

--- 30_danskcargo/danskcargo_sql.py
+++ 30_danskcargo/danskcargo_sql.py
@@ -4,6 +4,7 @@ from danskcargo_data import Container, Base
 
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
+from sqlalchemy import create_engine, select, update, delete
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -23,7 +24,6 @@ def create_test_data():
         session.add_all(new_items)
         session.commit()
 
-
 def select_all(classparam):
     with Session(engine) as session:
         records = session.scalars(select(classparam))
@@ -38,7 +38,30 @@ def get_record(classparam, record_id):
     return records
 
 
+def create_record(record):
+    with Session(engine) as session:
+        record.id = None
+        session.add(record)
+        session.commit()
 
+
+# region container
+def update_container(container):
+    with Session(engine) as session:
+        session.execute(update(Container).where(Container.id == container.id).values(weight=container.weight, destination=container.destination))
+        session.commit()
+
+def delete_hard_container(container):
+    with Session(engine) as session:
+        session.execute(delete(Container).where(Container.id == container.id))
+        session.commit()
+
+def delete_soft_container(container):
+    with Session(engine) as session:
+        session.execute(update(Container).where(Container.id == container.id).values(weight=-1, destination=container.destination))
+        session.commit()
+
+# endregion container
 
 
 

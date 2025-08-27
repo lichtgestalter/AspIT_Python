3b606ec1a1f3985eed76929699dde4ccf07f2fcb
lichtgestalter <uli.scheuss@gmail.com>
2025-08-27 09:37:03 +0200

    .

---
+++ 90_English/0250_factorial_exercise.py
@@ -0,0 +1,42 @@
+"""
+Exercise "factorial":
+
+As always, read the entire task description carefully before you begin solving the problem.
+
+Copy this file to your own solution folder. Write your solution into the copy.
+
+-------
+
+Write a function named "factorial".
+Scroll down to find the place in this file where you should write the function.
+
+The factorial function should:
+    - have one parameter named number.
+    - calculate the factorial of this number and store the result in a variable named result.
+    - return the result.
+
+If you can't remember the definition of a factorial, read this: https://en.wikipedia.org/wiki/Factorial)
+
+In the main program:
+    - Write a line of code that stores the integer 5 in a variable called factorial_input.
+    - In the next line of code, call the factorial function with factorial_input as the argument,
+      and store the result of this function call in a variable named factorial_result.
+    - In the next line of code, print "5! = 120" using the variables factorial_input and factorial_result.
+
+Run the program. "5! = 120" should be printed to the console.
+Change the first line in the main program to "factorial_input = 6".
+Run the program again.
+This time, "6! = 720" should be printed to the console.
+
+If you get stuck, ask Google, the other students, an AI, or the teacher.
+
+When your program is finished, push it to your GitHub repository.
+Then, continue with the next file.
+"""
+
+
+#  Write your function below this line.
+
+
+# Here starts the main program. From the main program you can call your functions.
+
